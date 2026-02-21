287635eb5cb5a884d6ea61b05cde9493b894e278
Darkone Linux <darkone@darkone.yt>
2026-02-21 12:11:06 -0300

    FR/EN parts reorganized + links reviews

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,61 +1,69 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import rehypeExternalLinks from 'rehype-external-links';
-import { targetBlank } from './src/plugins/targetBlank';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import rehypeExternalLinks from "rehype-external-links";
+import { targetBlank } from "./src/plugins/targetBlank";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://darkone-linux.github.io',
-	trailingSlash: "always",
-	build: {
-		assets: 'astro',
-	},
-	markdown: {
-		rehypePlugins: [
-			[targetBlank, { domain: 'darkone-linux.github.io' }],
-			[
-				rehypeExternalLinks,
-				{
-				  content: { type: 'text', value: '\u{00A0}\u{1F855}' }
-				}
-			],
-		],
-	},
-	integrations: [
-		starlight({
-			title: "Darkone NixOS Framework",
-			favicon: '/favicon.svg',
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			locales: {
-				root: {
-					label: 'English',
-					lang: 'en-US',
-				},
-				fr: {
-					label: 'Français',
-					lang: 'fr-FR',
-				},
-			},
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/darkone-linux/darkone-nixos-framework' },
-			],
-			logo: {
-				src: './src/assets/nix-logo.svg',
-			},
-			sidebar: [
-				{
-					label: 'Documentation',
-					autogenerate: { directory: 'doc' },
-				},
-				{
-					label: 'References',
-					autogenerate: { directory: 'ref' },
-				},
-				{ label: 'Changelog', slug: 'changelog' },
-				{ label: 'Thanks!', slug: 'thanks' },
-			],
-		}),
-	],
+  site: "https://darkone-linux.github.io",
+  trailingSlash: "always",
+  build: {
+    assets: "astro",
+  },
+  markdown: {
+    rehypePlugins: [
+      [targetBlank, { domain: "darkone-linux.github.io" }],
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: "text", value: "\u{00A0}\u{1F855}" },
+        },
+      ],
+    ],
+  },
+  redirects: {
+    "/": "/en/",
+    "/ref/[...slug]/": "/en/ref/[...slug]/",
+    "/doc/[...slug]/": "/en/doc/[...slug]/",
+  },
+  integrations: [
+    starlight({
+      title: "Darkone NixOS Framework",
+      favicon: "/favicon.svg",
+      customCss: ["./src/styles/custom.css"],
+      defaultLocale: "en",
+      locales: {
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        fr: {
+          label: "Français",
+          lang: "fr",
+        },
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/darkone-linux/darkone-nixos-framework",
+        },
+      ],
+      logo: {
+        src: "./src/assets/nix-logo.svg",
+      },
+      sidebar: [
+        {
+          label: "Documentation",
+          autogenerate: { directory: "doc" },
+        },
+        {
+          label: "References",
+          autogenerate: { directory: "ref" },
+        },
+        { label: "Changelog", slug: "changelog" },
+        { label: "Thanks!", slug: "thanks" },
+      ],
+    }),
+  ],
 });
